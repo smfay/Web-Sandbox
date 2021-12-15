6c8a4e27d91e18b0deef3aa33f6105df4ea2fe5f
Sean Fay <saltycrackersagain@gmail.com>
2021-12-14 21:44:29 -0500

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
 // board dimensions
 let boardWidth = 10;
 let boardHeight = 10;
+let shipLimit = 5;
 
 let humanShips = [];
 let compShips = [];
@@ -17,11 +18,11 @@ for(let i = 0; i < 5; i+=1) {
     compShips.push(String(point));
 }
 
-function initGrid() {
+function initGrid(id, onClick) {
     for(let y = 0; y < boardHeight; y+=1) {
         let rowElement = document.createElement("div")
         rowElement.classList.add("row")
-        let gridElement = document.getElementById("human-grid")
+        let gridElement = document.getElementById(id)
         gridElement.appendChild(rowElement)
         
         for(let x = 0; x < boardHeight; x+=1) {
@@ -29,26 +30,32 @@ function initGrid() {
             square.classList.add("square")
             square.setAttribute("x", x)
             square.setAttribute("y", y)
-            square.addEventListener("click", handleSelect)
+            square.setAttribute("id", id)
+
+            square.addEventListener("click", onClick)
+
             rowElement.appendChild(square)
         }
 
     }
 }
 
+function startGame() {
+
+}
+
 function handleSelect(event) {
-    console.log(event);
     let x = event.target.getAttribute("x")
     let y = event.target.getAttribute("y")
     let point = { x: x, y: y }
     event.target.removeEventListener("click", handleSelect)
-
-
     humanShips.push(point);
-    console.log(x,y,humanShips);
     event.srcElement.classList.add("occupied")
+    if(humanShips.length == shipLimit) {
+        startGame();
+    }
 }
 
-initGrid();
+initGrid("human-grid", handleSelect);
 console.log(compShips);
 console.log(humanShips);
